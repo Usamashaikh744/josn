60d420c8b88274d51fc1750d3bc5cce889527a4a
Usama Shaikh <48800317+Usamashaikh744@users.noreply.github.com>
2019-07-11 15:43:22 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,80 @@
+let students = [
+    {
+        name: "sharique",
+        height: 6,
+        age: 19,
+        contactNumber: 0315202122,
+        houseNumber: 1729/251,
+        city: "karachi",
+        qualification: "matric",
+        jobExperience:  "oneYear",
+        Job: "finance",
+        profession: "mechanic" 
+    },
+
+    {
+        name: "shafique",
+        height: 7,
+        age: 20,
+        contactNumber: 031545124,
+        houseNumber: 1729/252,
+        city: "laore",
+        qualification: "entre",
+        jobExperience:  "twoYears",
+        Job: "accountant",
+        profession: "labour" 
+    },
+
+    {
+        name: "amir",
+        height: 8,
+        age: 21,
+        contactNumber: 031020450,
+        houseNumber: 1729/253,
+        city: "islamabad",
+        qualification: "graduate",
+        jobExperience:  "threeYear",
+        Job: "itManager",
+        profession: "it"
+    },
+
+    {
+        name: "Latif",
+        height: 9,
+        age: 29,
+        contactNumber: 031254452,
+        houseNumber: 1729/253,
+        city: "bahawalpur",
+        qualification: "m.b.a",
+        jobExperience:  "fourYears",
+        Job: "managment",
+        profession: "rider",
+        
+    },
+
+    {
+        name: "shoukat",
+        height: 11,
+        age: 25,
+        contactNumber: 03251456,
+        houseNumber: 1729/254,
+        city: "sialkot",
+        qualification: "m.s.c",
+        jobExperience:  "fiveYear",
+        Job: "teacher",
+        profession: "cycling",
+    },
+
+    {
+        name: "arbaz",
+        height: 15,
+        age: 35,
+        contactNumber: 032214545,
+        houseNumber: 1729/255,
+        city: "faisalabad",
+        qualification: "m.b.a",
+        jobExperience:  "sixYear",
+        Job: "departManager", 
+        profession: "booksreading"
+    },
+];
